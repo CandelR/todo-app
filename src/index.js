ab507cee1b86da65bea71a232fe23c0727c7a542
Ramon <ramon@MacBook-Pro-de-Ramon.local>
2020-07-21 16:27:12 +0200

    added react router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import configureStore from "./configureStore";
 import Root from './components/Root';
+
 const store = configureStore();
-
-
 
 ReactDOM.render(
  <Root store = {store} />, document.getElementById("root"));
