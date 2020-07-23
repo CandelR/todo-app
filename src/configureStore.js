077659c794f58aeec2f2eeb8bf8ed99624bf29ae
Ramon <ramon@MacBook-Pro-de-Ramon.local>
2020-07-23 09:55:33 +0200

    middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,42 +1,50 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+import { createLogger } from "redux-logger";
 import todoApp from "./reducers";
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  if (!console.group) {
-    return rawDispatch;
-  }
-  return (action) => {
-    console.group(action.type);
-    console.log("%c prev state", 'color: gray', store.getState());
-    console.log("%c action", 'color: blue', action);
-    const returnValue = rawDispatch(action);
-    console.log("%c next state", 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
+//-----MIDDLEWARE MANUALLY IMPLEMENTED------------
+// const logger = (store) => {
+//   return (next) => {
+//     if (!console.group) {
+//       return next;
+//     }
+//     return (action) => {
+//       console.group(action.type);
+//       console.log("%c prev state", "color: gray", store.getState());
+//       console.log("%c action", "color: blue", action);
+//       const returnValue = next(action);
+//       console.log("%c next state", "color: green", store.getState());
+//       console.groupEnd(action.type);
+//       return returnValue;
+//     };
+//   };
+// };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
-};
-
+// const promise = (store) => {
+//   return (next) => {
+//     return (action) => {
+//       if (typeof action.then === "function") {
+//         return action.then(next);
+//       }
+//       return next(action);
+//     };
+//   };
+// };
+// const wrapDispatchWithMiddlewares = (store, middlewares) => {
+//   middlewares.slice().reverse().forEach((middlewares) => {
+//     store.dispatch = middlewares(store)(store.dispatch);
+//   });
+// };
+// -------------------------------------------
 const configureStore = () => {
-
-  const store = createStore(todoApp);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== "production") {
-    store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(createLogger());
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
-  return store;
+  return createStore(todoApp, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
